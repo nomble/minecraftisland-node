6eb2002e971b1fee6219a6a91e9825c68f67c51e
necmettindev <necmettinayata@icloud.com>
2023-09-17 12:25:47 +0300

    chore: remove unused sum and subtract files and tests The sum.ts, subtract.ts, sum.test.ts, and subtract.test.ts files and their corresponding tests have been removed. These files were no longer needed and were deleted to clean up the codebase and remove unnecessary files.

--- src/sum.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import sum from './sum';
-
-describe('Sum', () => {
-  it('should successfully add a number', () => {
-    expect(sum(2, 1)).eq(3);
-  });
-});
