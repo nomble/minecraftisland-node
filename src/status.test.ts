bef57946196b05eda7f5e4454f54bb69525384df
necmettindev <necmettinayata@icloud.com>
2023-09-17 12:32:46 +0300

    test(status): add unit test for the status function A new unit test file `status.test.ts` has been added to test the `status` function. The test verifies that the `status` function returns the expected result when given a specific input. This test ensures that the `status` function is working correctly and returns the expected status object.

---
+++ src/status.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+import { status } from './status';
+
+describe('Status', () => {
+  it('should return status', async () => {
+    const res = await status({
+      address: 'mc.hypixel.net',
+      port: 25565,
+      type: 'java',
+    });
+
+    expect(res).toEqual({
+      online: true,
+      motd: '§aHypixel Network §c[1.8-1.16]',
+      players: {
+        max: 85000,
+        now: 103927,
+      },
+      version: 'Requires MC 1.8 / 1.16',
+      ping: 0,
+    });
+  });
+});
