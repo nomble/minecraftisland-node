7620134924c171d4ea107e0a4f99867444414735
necmettindev <necmettinayata@icloud.com>
2023-09-17 12:30:54 +0300

    feat(types.ts): add types for server status and icon requests The types.ts file has been added to the project to include necessary types for server status and icon requests. This improves the codebase by providing type safety and better documentation for these requests.

---
+++ src/types.ts
@@ -0,0 +1,96 @@
+export enum Server {
+  JAVA = 'java',
+  BEDROCK = 'bedrock',
+}
+
+export type StatusRequest = {
+  type: Server;
+  address: string;
+  port?: number;
+};
+
+export type IconRequest = {
+  address: string;
+};
+
+export type BaseResponse<T> = {
+  status: 'success' | 'error';
+  timestamp: number;
+  data: T;
+};
+
+export type PingResponse = 'online' | 'offline';
+
+export type JavaStatusOptions = {
+  query: boolean;
+};
+
+export type BaseStatusResponse = {
+  online: boolean;
+  host: string;
+  port: number;
+  eula_blocked: boolean;
+  retrieved_at: number;
+  expires_at: number;
+};
+
+export type JavaStatusPureResponse = {
+  version?: {
+    name_raw: string;
+    name_clean: string;
+    name_html: string;
+    protocol: number;
+  } | null;
+  players?: {
+    online: number;
+    max: number;
+    list: {
+      uuid: string;
+      name_raw: string;
+      name_clean: string;
+      name_html: string;
+    }[];
+  };
+  motd?: {
+    raw: string;
+    clean: string;
+    html: string;
+  };
+  icon?: string | null;
+  mods?: {
+    name: string;
+    version: string;
+  }[];
+  plugins?: {
+    name: string;
+    version: string;
+  }[];
+  software?: string;
+};
+
+export type JavaStatusResponse = BaseStatusResponse & {
+  Extra?: JavaStatusPureResponse;
+};
+
+export type BedrockStatusPureResponse = {
+  version?: {
+    name: string;
+    protocol: number;
+  };
+  players?: {
+    online: number;
+    max: number;
+  };
+  motd?: {
+    raw: string;
+    clean: string;
+    html: string;
+  };
+  gamemode?: string;
+  server_id?: string;
+  edition?: 'MCPE' | 'MCEE';
+};
+
+export type BedrockStatusResponse = BaseStatusResponse & {
+  Extra?: BedrockStatusPureResponse;
+};
